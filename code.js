01771a87ab9166a38dc06373d5dbd2563c7c734d
TheRedVillager <49311312+TheRedVillager@users.noreply.github.com>
2021-08-01 16:32:53 +0200

    Initial

---
+++ code.js
@@ -0,0 +1,36 @@
+function generate(){
+    var length = document.getElementById("length").value;
+    var result = "";
+    var characters = "";
+    
+    if(document.getElementById("lowercase").checked == true){
+        characters += "abcdefghijklmnopqrstuvwxyz"
+    }
+
+    if(document.getElementById("uppercase").checked == true){
+        characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    }
+
+    if(document.getElementById("numbers").checked == true){
+        characters += "0123456789"
+    }
+
+    if(document.getElementById("symbols").checked == true){
+        characters += "$%&?*+-!~#.,"
+    }
+
+    for(var i = 0; i < length; i++){
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+
+
+    document.getElementById("result").value = result;
+
+    if(!(length > 0)){
+        document.getElementById("result").value = "The password must atleast be one character long";
+    }
+    else if(result == ""){
+        document.getElementById("result").value = "Atleast one box must be ticked";
+    }
+
+}
